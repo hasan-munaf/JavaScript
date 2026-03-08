1b4f2c1b089d4f9afabbbedc952f077618da4c5b
hasan-munaf <2k23.csiot2312389@gmail.com>
2026-03-08 17:59:37 +0000

    first commit

---
+++ asynchronous.js
@@ -0,0 +1,16 @@
+// Asynchronous programming can be refered to as a form of programming in which a fix pattern is not followed 
+
+// console.log("Phantom Here🥷");
+
+function sayHello() {
+    // console.log(`Hello from the Developer's end !!!`);
+} 
+
+setTimeout(() => {
+   sayHello() 
+}, 6000);
+ 
+for (let index = 0; index < 10; index++) {
+    // console.log(index);       // this is a form of synchronous programming which means that all the object in the programming follows a pattern .
+    
+}
